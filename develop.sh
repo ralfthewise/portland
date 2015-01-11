b5249b0ede1a723fea1da1dc3b00fa95a12774b7
Tim Garton <garton.tim@gmail.com>
2015-01-10 20:32:26 -0800

    some improvements to docker development config

--- develop.sh
+++ develop.sh
@@ -3,8 +3,8 @@
 ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "${ROOT}" > /dev/null
 docker build --rm -t portland "$ROOT/docker/development"
-#docker run --rm -t -i -u `id -u` -v /var/run/docker.sock:/var/run/docker.sock -p 3000:3000 -p 3001:3001 -v "$ROOT":/app -w /app portland "$@"
-docker run --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -p 8000:80 -v "$ROOT":/app -w /app --name=portland portland "$@"
+#docker run --rm -it -u `id -u` -v /var/run/docker.sock:/var/run/docker.sock -p 3000:3000 -p 3001:3001 -v "$ROOT":/app -w /app portland "$@"
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -p 8000:80 -v "$ROOT":/app -w /app --name=portland portland "$@"
 docker rm $(docker ps -a -q) 2> /dev/null
 #docker rmi $(docker images -q --filter "dangling=true") 2> /dev/null
 popd > /dev/null
--- docker/development/run.sh
+++ docker/development/run.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+bundle install --path vendor/bundle --binstubs vendor/bundle/bin --without production
 bundle exec eye load eye/development.eye
 
-#fancy status dashboard
-tmux new-session -d "bash -l -c \"bundle exec eye watch\""
-tmux split-window -h -p 65 "bash -c \"trap 'tmux kill-session' SIGINT SIGTERM; tail -f /tmp/eye.log /tmp/rails.log /app/log/development.log /tmp/ws_server.log /app/log/websocket_rails.log /app/log/websocket_rails_server.log /tmp/docker_stream_events.log /tmp/terminal.log /var/log/nginx/error.log /var/log/nginx/access.log /var/log/redis/redis-server.log\""
-tmux -2 attach-session
+LOG_FILES=(
+  /var/log/nginx/error.log
+  /var/log/nginx/access.log
+  /var/log/redis/redis-server.log
+  /tmp/eye.log
+  /tmp/rails.log
+  /tmp/ws_server.log
+  /tmp/docker_stream_events.log
+  /tmp/terminal.log
+  /app/log/development.log
+  /app/log/websocket_rails.log
+  /app/log/websocket_rails_server.log
+)
 
-#or to just tail things
-#tail -f /tmp/eye.log /tmp/rails.log /app/log/development.log /tmp/terminal.log /var/log/nginx/error.log /var/log/nginx/access.log
+#if [ $(tty) == "/dev/console" ]; then
+if [ -t 1 ]; then
+  #we're attached to a terminal, do our fancy dashboard
+  tmux new-session -d "bash -l -c \"bundle exec eye watch\""
+  tmux split-window -h -p 65 "bash -c \"trap 'tmux kill-session' SIGINT SIGTERM; tail -f ${LOG_FILES[*]}\""
+  tmux -2 attach-session
+else
+  #we're not attached to a terminal, just do simple logging
+  tail -f ${LOG_FILES[*]}
+fi
 
 bundle exec eye quit -s
